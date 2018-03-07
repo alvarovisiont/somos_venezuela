08f36f4f0b1f2659993a0716fa21812b61c8e7ef
sheylert <sheylert@gmail.com>
2018-03-07 18:45:12 -0400

    ss

--- application/models/Menumodel.php
+++ application/models/Menumodel.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Menu extends CI_Model {
+class Menumodel extends CI_Model {
 
 	public function __construct()
     { 
