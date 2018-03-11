473c8fa7c0474e80cbd772bdaf8ecb9e25c8639a
sheylert <sheylert@gmail.com>
2018-03-11 13:38:08 -0400

    cambios

--- application/helpers/menu_admin_helper.php
+++ application/helpers/menu_admin_helper.php
@@ -1,24 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
 //si no existe la función invierte_date_time la creamos
-if(!function_exists('menu'))
-{
-    //formateamos la fecha y la hora, función de cesarcancino.com
-
- function menu()
- {
- 	 $CI =& get_instance();
-     $CI->load->library('session');
-
- 	 $varhtml = $CI->session->userdata('menu_usuario');
-
- 	 $varhtml .= '<div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
-					<i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
-				</div>';
-     return $varhtml;
- }
-}
-
 
 if(!function_exists('menu_admin'))
 {
