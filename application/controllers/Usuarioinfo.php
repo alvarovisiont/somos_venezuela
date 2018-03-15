e40af5f67c66e9eac3710833cf1ae5b0857b9a29
sheylert <sheylert@gmail.com>
2018-03-15 15:34:08 -0400

    cambios

--- application/controllers/Usuarioinfo.php
+++ application/controllers/Usuarioinfo.php
@@ -82,6 +82,17 @@ class Usuarioinfo extends CI_Controller {
 
     public function uploadImg()
     {
+      $config = [];
+
+      $config['upload_path'] = './assets_sistema/images/avatars';
+      $config['allowed_types'] = 'jpg|png|jpeg';
+      $config['max_size'] = '2048';
+      $config['encrypt_name'] = TRUE;
+      $config['overwrite'] = TRUE;
+
+       $this->load->library('upload');
+
+       $this->upload->initialize($config);
 
       if (!$this->upload->do_upload('foto')) {
 
