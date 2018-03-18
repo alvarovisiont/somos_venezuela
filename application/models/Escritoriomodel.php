ae204d03745f7216c009492086a13f9656002823
Alvaro Guedez <alvarovisiont@gmail.com>
2018-03-18 16:18:37 -0400

    db work helper2

---
+++ application/models/Escritoriomodel.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Escritoriomodel extends CI_Model {
+
+   public function __construct()
+    {
+      //parent::__construct();
+      //Codeigniter : Write Less Do More
+      //Revisar 
+    }
+
+    public function dashboard_data()
+    {
+        //$db_admin = $this->load->database($this->session->userdata('bd_activa'), TRUE);
+
+        $db_admin = $this->load->database('admin21', TRUE);
+
+        $sql = "SELECT m.municipio, m.id_municipio from municipios as m where id_estado = 17";
+
+        return $db_admin->query($sql)->result();
+
+        $db_admin->close();
+
+    }
+}
+
+?>    
---
+++ application/views/escritorio/index.php
@@ -0,0 +1,32 @@
+<div class="page-header">
+	<h1>
+		Dashboard
+		<small>
+			<i class="ace-icon fa fa-angle-double-right"></i>
+			Menú
+		</small>
+	</h1>
+</div><!-- /.page-header -->
+
+
+<div class="row no-gutters">
+    <div class="row no-gutters">
+		<div class="col-md-12 col-sm-12">
+			<table class="table table-bordered table-responsive">
+				<thead>
+					<tr>
+						<th class="text-center">Municipio</th>
+						<th class="text-center">Centros Medicos</th>
+						<th class="text-center">Registradores</th>
+						<th class="text-center">Doctores</th>
+						<th class="text-center">Censados</th>
+						<th class="text-center">Ver</th>
+					</tr>
+				</thead>
+				<tbody class="text-center">
+					
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
